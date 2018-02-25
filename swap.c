3ce58af95bc690a0cdefc31438bdc7c56b7900bc
varadaraju <varadaraju.dvg@gmail.com>
2018-02-25 14:10:31 +0530

    swap two numbers

---
+++ swap.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1,num2;
+	int temp;
+
+	printf("Enter Numbers to Swap:\n");
+	scanf("%d%d", &num1, &num2);
+
+	printf("Before swap:\n\tnum1 = %d\n\tnum2 = %d\n",num1,num2);
+	
+	/*
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+	*/
+
+	/*
+	num1 = num1 + num2;
+	num2 = num1 - num2;
+	num1 = num1 - num2;
+	*/
+
+	num1 = num1 * num2;
+	num2 = num1 / num2;
+	num1 = num1 / num2;
+
+	printf("After swap:\n\tnum1 = %d\n\tnum2 = %d\n",num1,num2);
+
+	return 0;
+}
