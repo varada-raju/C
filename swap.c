614bb3ce0fa5aca2218a874fc75fcae063311694
varadaraju <varadaraju.dvg@gmail.com>
2018-02-25 19:43:25 +0530

    swap two numbers with or without temp variable

--- swap.c
+++ swap.c
@@ -9,13 +9,15 @@ int main()
 	scanf("%d%d", &num1, &num2);
 
 	printf("Before swap:\n\tnum1 = %d\n\tnum2 = %d\n",num1,num2);
-	
+
+	//with temp variable	
 	/*
 	temp = num1;
 	num1 = num2;
 	num2 = temp;
 	*/
 
+	//without temp variable
 	/*
 	num1 = num1 + num2;
 	num2 = num1 - num2;
